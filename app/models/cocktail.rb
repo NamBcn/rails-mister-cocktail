5986cd00525ff1804314fc77c91d565ec7f3d201
Nam Hai Chau <namhai.chau@gmail.com>
2019-05-16 13:11:38 +0200

    added correct db relations

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,7 @@
 class Cocktail < ApplicationRecord
-  validates :name, uniqueness: true, presence: true
-  has_many :doses, :dependent => :destroy
-  has_many :ingredients, :dependent => :destroy
+  validates :name, uniqueness: true, length: { minimum: 1 }
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
   # validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
   # has_many :reviews,
   # validates_associated :reviews
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,3 +1,9 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, length: { minimum: 1 }
+  validates :ingredient, uniqueness: { scope: :cocktail }
+  # has_many :doses, :dependent => :destroy
+  # has_many :ingredients, :dependent => :destroy
+  # belongs_to :cocktail
 end
